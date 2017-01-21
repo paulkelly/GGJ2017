21b66f783b3552f53cab9cfcba4d86410c8189c5
Paul Kelly <pkelly429@gmail.com>
2017-01-21 13:30:48 +0000

    Adding volume ui.

--- Assets/Scripts/Volume/PipController.cs
+++ Assets/Scripts/Volume/PipController.cs
@@ -2,15 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipController : MonoBehaviour {
+public class PipController : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float Volume;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private List<VolumePip> _pips = new List<VolumePip>();
+	void Start ()
+    {
+        foreach(VolumePip p in GetComponentsInChildren<VolumePip>())
+        {
+            _pips.Add(p);
+        }
+
+        for(int i=0; i<_pips.Count; i++)
+        {
+            SetPipColor(_pips[i], 1- ((float)i / (float)_pips.Count));
+        }
+    }
+
+    private void SetPipColor(VolumePip pip, float normalPosition)
+    {
+        Color c = Color.yellow;
+        float pos = normalPosition;
+        if(normalPosition < 0.5f)
+        {
+            pos = normalPosition / 0.5f;
+            c = Color.Lerp(Color.green, Color.yellow, normalPosition/0.5f);
+        }
+        else if(normalPosition > 0.5f)
+        {
+            pos = (normalPosition / 0.5f) - 0.5f;
+            c = Color.Lerp(Color.yellow, Color.red, (normalPosition/0.5f)-1);
+        }
+        pip.SetColor(c);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < _pips.Count; i++)
+        {
+            //1-Volume because scripts are in back to front order
+            bool on = ((float)i / (float)_pips.Count) >= (1 - Volume);
+            if(on)
+            {
+                _pips[i].Show();
+            }
+            else
+            {
+                _pips[i].Hide();
+            }
+        }
+    }
+
 }
--- Assets/Scripts/Volume/VolumePip.cs
+++ Assets/Scripts/Volume/VolumePip.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class VolumePip : MonoBehaviour {
+public class VolumePip : MonoBehaviour
+{
+    private Image _image;
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SetColor(Color c)
+    {
+        _image.color = c;
+        Hide();
+    }
+
+    public void Show()
+    {
+        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 1);
+    }
+
+    public void Hide()
+    {
+        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 0);
+    }
 }
