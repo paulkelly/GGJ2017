ffa0df72fb422ca94687e98b2099b7c2ed83c56c
Paul Kelly <pkelly429@gmail.com>
2017-01-21 14:25:39 +0000

    Updating scene and prefabs.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Player : MonoBehaviour
+{
+    public float GoatVolume;
+    public float ManVolume;
 }
