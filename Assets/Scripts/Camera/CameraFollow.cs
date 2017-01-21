ab1a70464c714254a47f14042659dea04005d37e
Paul Kelly <pkelly429@gmail.com>
2017-01-21 17:48:41 +0000

    Fixing up camera movement a bit.

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    private const float SmoothTime = 3f;
+
+    private Vector3 _initalOffset;
+    private Vector3 _velocity;
+
+    private void Awake()
+    {
+        _initalOffset = new Vector3(transform.position.x - Target.position.x, 0, 0);
+    }
+
+    private void Update()
+    {
+        Vector3 target = new Vector3(Target.position.x, transform.position.y, Target.position.z) + _initalOffset;
+        float maxSpeed = Mathf.Max(0.5f, 2*Vector3.Distance(transform.position, target));
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref _velocity, SmoothTime/maxSpeed, maxSpeed, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/ForwardRaycast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardRaycast : MonoBehaviour
+{
+    public LayerMask Mask;
+
+    public bool HasHit;
+    public Vector3 HitPoint;
+
+    private const float Max = 4;
+
+	private void FixedUpdate ()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        HasHit = Physics.Raycast(ray, out hit, Max, Mask);
+        if(HasHit)
+        {
+            HitPoint = hit.point;
+        }
+	}
+}
